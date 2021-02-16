ea2ee50242b8e9549832186d4aa170fac04d9d9d
Ove Lartelius <ove_lartelius@hotmail.com>
2021-02-16 23:44:55 +0100

    :beer: Try to get EpinovaDxpDeploymentUtil running

---
+++ src/ExpectStatus/ps_modules/EpinovaDxpDeploymentUtil/EpinovaDxpDeploymentUtil.psm1
@@ -0,0 +1,14 @@
+<#
+
+
+.DESCRIPTION
+    Help functions for Epinova DXP deployment extension.
+#>
+
+Set-StrictMode -Version Latest
+function Write-DxpHostInfo() {
+    $version = Get-Host | Select-Object Version
+    Write-Host $version
+}
+
+Export-ModuleMember -Function Write-DxpHostInfo
