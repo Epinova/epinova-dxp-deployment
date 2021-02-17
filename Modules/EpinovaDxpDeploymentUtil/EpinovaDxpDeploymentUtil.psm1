b7099c1fd2904d71de6618c5dce8e45ec9d9cfb9
Ove Lartelius <ove_lartelius@hotmail.com>
2021-02-17 23:43:32 +0100

    :sparkles: Added new functions in Util module

--- Modules/EpinovaDxpDeploymentUtil/EpinovaDxpDeploymentUtil.psm1
+++ Modules/EpinovaDxpDeploymentUtil/EpinovaDxpDeploymentUtil.psm1
@@ -56,7 +56,7 @@ function Test-IsGuid
 
 function Test-DxpProjectId
 {
-        <#
+    <#
     .SYNOPSIS
         Test a DXP project id.
 
@@ -83,4 +83,92 @@ function Test-DxpProjectId
     }
 }
 
-Export-ModuleMember -Function @( 'Write-DxpHostVersion', 'Test-DxpProjectId' )
+function Get-DxpDateTimeStamp{
+    <#
+    .SYNOPSIS
+        Create DateTime stamp in correct format.
+
+    .DESCRIPTION
+        Create DateTime stamp in yyyy-MM-ddTHH:mm:ss format.
+
+    .EXAMPLE
+        Get-DxpDateTimeStamp
+
+        You will get the DateTime now in the format ex: '2021-02-20T14:34:22'.
+    #>
+    $dateTimeNow = Get-Date -Format "yyyy-MM-ddTHH:mm:ss"
+    return $dateTimeNow
+}
+
+function Invoke-DxpProgress {
+    <#
+    .SYNOPSIS
+        Write the progress of a operation in the Episerver DXP environment to the host.
+
+    .DESCRIPTION
+        Write the progress of a operation in the Episerver DXP environment to the host.
+
+    .PARAMETER projectId
+        Project id for the project in Episerver DXP.
+
+    .PARAMETER deploymentId
+        Deployment id for the specific deployment in Episerver DXP that you want to show the progress for.
+
+    .PARAMETER percentComplete
+        The initialized percentComplete value that we got from the invoke of the operation.
+
+    .PARAMETER expectedStatus
+        The expectedStatus that the deployment should get when done/before timeout.
+
+    .PARAMETER timeout
+        The maximum time that the progress should run. When the script has timeout if will stop.
+
+    .EXAMPLE
+        $status = Invoke-DxpProgress -projectid $projectId -deploymentId $deploymentId -percentComplete $percentComplete -expectedStatus $expectedStatus -timeout $timeout
+
+    .EXAMPLE
+        $status = Invoke-DxpProgress -projectid '644b6926-39b1-42a1-93d6-3771cdc4a04e' -deploymentId '817b5df3-21cd-4080-adbd-6c211b71f34d' -percentComplete 0 -expectedStatus 'Success' -timeout 1800
+
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $projectId,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $deploymentId,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [int] $percentComplete,
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string] $expectedStatus,
+        [Parameter(Mandatory = $true)]
+        [int] $timeout
+    )
+
+    $sw = [Diagnostics.Stopwatch]::StartNew()
+    $sw.Start()
+    while ($percentComplete -le 100) {
+        $status = Get-EpiDeployment -ProjectId $projectId -Id $deploymentId
+        if ($percentComplete -ne $status.percentComplete) {
+            $percentComplete = $status.percentComplete
+            Write-Host $percentComplete "%. Status: $($status.status). ElapsedSeconds: $($sw.Elapsed.TotalSeconds)"
+        }
+        if ($percentComplete -le 100 -or $status.status -ne $expectedStatus) {
+            Start-Sleep 5
+        }
+        if ($sw.Elapsed.TotalSeconds -ge $timeout) { break }
+        if ($status.percentComplete -eq 100 -and $status.status -eq $expectedStatus) { break }
+    }
+
+    $sw.Stop()
+    Write-Host "Stopped iteration after $($sw.Elapsed.TotalSeconds) seconds."
+
+    $status = Get-EpiDeployment -ProjectId $projectId -Id $deploymentId
+    $status
+    return $status
+}
+
+Export-ModuleMember -Function @( 'Write-DxpHostVersion', 'Test-DxpProjectId', 'Get-DxpDateTimeStamp', 'Invoke-DxpProgress' )
