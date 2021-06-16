e3b7753e1e50ecb598e225cb0d7b913dcab1649a
Ove Lartelius <ove_lartelius@hotmail.com>
2021-06-16 13:34:36 +0200

    try19

--- src/ImportModuleSandbox/ImportModuleSandbox.ps1
+++ src/ImportModuleSandbox/ImportModuleSandbox.ps1
@@ -41,102 +41,103 @@ try {
 
     Write-Host $PSVersionTable.PSVersion
 
-    Write-Host "Start test ...v1.12.19"
+    Write-Host "Start test ...v1.12.20"
 
-    Get-Module -ListAvailable
+    Get-Module -Name "Azure.Storage" -ListAvailable
+    Get-Module -Name "Az.Storage" -ListAvailable
     #Script     2.1.0      Azure.Storage
 
-    $azureModuleLoaded = Get-Module -Name "Azure.Storage"
-    if ($null -eq $azureModuleLoaded) { 
-        Write-Host "Get-Module `$azureModuleLoaded=null" 
-        $azureModuleLoaded = $false
-    }
-    else {
-        $azureModuleLoaded
-        Write-Host $azureModuleLoaded.Version
-        Write-Host $azureModuleLoaded.Version.Major
-    }
-    $azureModuleLoaded = Find-Module -Name "Azure.Storage"
-    if ($null -eq $azureModuleLoaded) { 
-        Write-Host "Find-Module `$azureModuleLoaded=null" 
-        $azureModuleLoaded = $false
-    }
-    else {
-        $azureModuleLoaded
-        Write-Host $azureModuleLoaded.Version
-        Write-Host $azureModuleLoaded.Version.Major
-    }
-    $azModuleLoaded = Get-Module -Name "Az.Storage"
-    if ($null -eq $azModuleLoaded) { 
-        Write-Host "Get-Module `$azModuleLoaded=null" 
-        $azModuleLoaded = $false
-    }
-    else {
-        $azModuleLoaded
-        Write-Host $azModuleLoaded.Version
-        Write-Host $azModuleLoaded.Version.Major
-    }
-    $azModuleLoaded = Find-Module -Name "Az.Storage"
-    if ($null -eq $azModuleLoaded) { 
-        Write-Host "Find-Module `$azModuleLoaded=null" 
-        $azModuleLoaded = $false
-    }
-    else {
-        $azModuleLoaded
-        Write-Host $azModuleLoaded.Version
-        Write-Host $azModuleLoaded.Version.Major
-    }
+    # $azureModuleLoaded = Get-Module -Name "Azure.Storage"
+    # if ($null -eq $azureModuleLoaded) { 
+    #     Write-Host "Get-Module `$azureModuleLoaded=null" 
+    #     $azureModuleLoaded = $false
+    # }
+    # else {
+    #     $azureModuleLoaded
+    #     Write-Host $azureModuleLoaded.Version
+    #     Write-Host $azureModuleLoaded.Version.Major
+    # }
+    # $azureModuleLoaded = Find-Module -Name "Azure.Storage"
+    # if ($null -eq $azureModuleLoaded) { 
+    #     Write-Host "Find-Module `$azureModuleLoaded=null" 
+    #     $azureModuleLoaded = $false
+    # }
+    # else {
+    #     $azureModuleLoaded
+    #     Write-Host $azureModuleLoaded.Version
+    #     Write-Host $azureModuleLoaded.Version.Major
+    # }
+    # $azModuleLoaded = Get-Module -Name "Az.Storage"
+    # if ($null -eq $azModuleLoaded) { 
+    #     Write-Host "Get-Module `$azModuleLoaded=null" 
+    #     $azModuleLoaded = $false
+    # }
+    # else {
+    #     $azModuleLoaded
+    #     Write-Host $azModuleLoaded.Version
+    #     Write-Host $azModuleLoaded.Version.Major
+    # }
+    # $azModuleLoaded = Find-Module -Name "Az.Storage"
+    # if ($null -eq $azModuleLoaded) { 
+    #     Write-Host "Find-Module `$azModuleLoaded=null" 
+    #     $azModuleLoaded = $false
+    # }
+    # else {
+    #     $azModuleLoaded
+    #     Write-Host $azModuleLoaded.Version
+    #     Write-Host $azModuleLoaded.Version.Major
+    # }
 
-    if (-not ($azureModuleLoaded -or $azModuleLoaded)) {
-        try {
-            #$null = Import-Module -Name "Az.Storage" -Scope CurrentUser -Repository PSGallery -MinimumVersion 3.5.0 -Force -AllowClobber -ErrorAction Stop
-            Install-Module -Name "Az.Storage" -Scope CurrentUser -Repository PSGallery -MinimumVersion 3.5.0 -Force -AllowClobber
-            #$null = Import-Module -Name "Az.Storage" -ErrorAction Stop
-            $azModuleLoaded = $true
-            Write-Host "Az.Storage is imported."
-        }
-        catch {
-            Write-Host "Exception caught from task: $($_.Exception.ToString())"
-            Write-Host "Tried to find 'Az.Storage', module couldn't be imported."
-        }
-    }
+    # if (-not ($azureModuleLoaded -or $azModuleLoaded)) {
+    #     try {
+    #         #$null = Import-Module -Name "Az.Storage" -Scope CurrentUser -Repository PSGallery -MinimumVersion 3.5.0 -Force -AllowClobber -ErrorAction Stop
+    #         Install-Module -Name "Az.Storage" -Scope CurrentUser -Repository PSGallery -MinimumVersion 3.5.0 -Force -AllowClobber
+    #         #$null = Import-Module -Name "Az.Storage" -ErrorAction Stop
+    #         $azModuleLoaded = $true
+    #         Write-Host "Az.Storage is imported."
+    #     }
+    #     catch {
+    #         Write-Host "Exception caught from task: $($_.Exception.ToString())"
+    #         Write-Host "Tried to find 'Az.Storage', module couldn't be imported."
+    #     }
+    # }
 
-    #$result1 = Get-InstalledModule -Name "Azure.Storage"
-    #Write-Host $result1.Version
-    #$result2 = Get-InstalledModule -Name "Az.Storage"
-    #Write-Host $result2.Version
+    # #$result1 = Get-InstalledModule -Name "Azure.Storage"
+    # #Write-Host $result1.Version
+    # #$result2 = Get-InstalledModule -Name "Az.Storage"
+    # #Write-Host $result2.Version
 
-    if (-not ($azureModuleLoaded -or $azModuleLoaded)) {
-        try {
-            #Import-Module -Name "Azure.Storage" -ErrorAction Stop
-            #$null = Import-Module -Name "Azure.Storage" -MinimumVersion 4.4.0 -ErrorAction Stop
-            #$null = Import-Module -Name "Azure.Storage" -MinimumVersion 4.4.0 -Scope CurrentUser -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
-            Install-Module -Name "Azure.Storage" -MinimumVersion 4.4.0 -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
-            $azureModuleLoaded = $true
-            Write-Host "Azure.Storage is imported."
-        }
-        catch {
-            Write-Host "Exception caught from task: $($_.Exception.ToString())"
-            Write-Host "Tried to find 'Azure.Storage', module couldn't be imported."
-        }
-    }
+    # if (-not ($azureModuleLoaded -or $azModuleLoaded)) {
+    #     try {
+    #         #Import-Module -Name "Azure.Storage" -ErrorAction Stop
+    #         #$null = Import-Module -Name "Azure.Storage" -MinimumVersion 4.4.0 -ErrorAction Stop
+    #         #$null = Import-Module -Name "Azure.Storage" -MinimumVersion 4.4.0 -Scope CurrentUser -Repository PSGallery -Force -AllowClobber -ErrorAction Stop
+    #         Install-Module -Name "Azure.Storage" -MinimumVersion 4.4.0 -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
+    #         $azureModuleLoaded = $true
+    #         Write-Host "Azure.Storage is imported."
+    #     }
+    #     catch {
+    #         Write-Host "Exception caught from task: $($_.Exception.ToString())"
+    #         Write-Host "Tried to find 'Azure.Storage', module couldn't be imported."
+    #     }
+    # }
 
-    if ($azModuleLoaded) {
-        Write-Host "Az"
-    }
-    elseif ($azureModuleLoaded) {
-        $azureModuleLoaded = Get-Module -Name "Azure.Storage"
-        if ($azureModuleLoaded.Version.Major -lt 4 -or ($azureModuleLoaded.Version.Major -eq 4 -and $azureModuleLoaded.Version.Minor -lt 4)) {
-            # Previous versions of Azure.Storage do not support SAS links with write-only permission.
-            throw "'Azure.Storage' version 4.4.0 or greater is required."
-        }
-        Write-Host "Azure"
-    }
-    else {
-        throw "'Az.Storage' or 'Azure.Storage' module is required to run this cmdlet."
-    }
+    # if ($azModuleLoaded) {
+    #     Write-Host "Az"
+    # }
+    # elseif ($azureModuleLoaded) {
+    #     $azureModuleLoaded = Get-Module -Name "Azure.Storage"
+    #     if ($azureModuleLoaded.Version.Major -lt 4 -or ($azureModuleLoaded.Version.Major -eq 4 -and $azureModuleLoaded.Version.Minor -lt 4)) {
+    #         # Previous versions of Azure.Storage do not support SAS links with write-only permission.
+    #         throw "'Azure.Storage' version 4.4.0 or greater is required."
+    #     }
+    #     Write-Host "Azure"
+    # }
+    # else {
+    #     throw "'Az.Storage' or 'Azure.Storage' module is required to run this cmdlet."
+    # }
 
-    Get-Module -ListAvailable
+    # Get-Module -ListAvailable
 
     Write-Host "End test ..."
 
