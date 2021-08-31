2854375aad6ddd8583523e1072bffbb0bd7a8ee6
Ove Lartelius <ove_lartelius@hotmail.com>
2021-08-31 22:52:21 +0200

    :bug: Fix param for buildnumber

--- BuildScripts/GetExtensionVersion.ps1
+++ BuildScripts/GetExtensionVersion.ps1
@@ -1,4 +1,9 @@
+[CmdletBinding()]
+Param(
+    $buildnumber
+)
+
 $version = (Get-Content src\vss-extension.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
-$extVersion = "v$version-$(Build.BuildNumber)"
+$extVersion = "v$version-$buildnumber"
 Write-Host "Try to set ExtensionVersion to $extVersion"
 Write-Host "##vso[task.setvariable variable=ExtensionVersion;]"$extVersion
