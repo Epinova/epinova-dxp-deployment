126b0d1c0ff7d2ef6539beb7ac1fa1e5dfce2988
Ove Lartelius <ove_lartelius@hotmail.com>
2021-09-01 22:21:52 +0200

    :art: update test rename script

--- BuildScripts/RenameTasksToTest.ps1
+++ BuildScripts/RenameTasksToTest.ps1
@@ -6,7 +6,19 @@ Write-Host "Root: $srcRootPath"
 
 $tasksfiles = Get-ChildItem -Path $srcRootPath -Filter task.json -Recurse -ErrorAction SilentlyContinue -Force
 foreach ($taskfile in $tasksfiles){
-  Write-Host "Task file: $taskfile"
+  Write-Host "Task file: $($taskfile.FullName)"
+  if (Test-Path $filePath){
+    $a = Get-Content $filePath -raw | ConvertFrom-Json
+    $newFriendlyName = "$($a.friendlyName)-TEST"
+    $a.friendlyName = "$newFriendlyName"
+    Write-Host "friendlyName: $newFriendlyName"
+    $newName = "$($a.name)-TEST"
+    $a.name = "$newName"
+    Write-Host "name: $newName"
+
+    #$a | ConvertTo-Json -depth 32| set-content $filePath
+    Write-Host "Updated: $($taskfile.FullName)"
+  }
 }
 
 # $dir = Get-ChildItem -Path $srcRootPath -Directory
