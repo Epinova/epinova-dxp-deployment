b9854d6f7ad215de6ecb3f785d399009cf41915e
Ove Lartelius <ove_lartelius@hotmail.com>
2021-09-01 22:10:08 +0200

    :rocket: Start update test rename script

--- BuildScripts/RenameTasksToTest.ps1
+++ BuildScripts/RenameTasksToTest.ps1
@@ -1,21 +1,30 @@
 Write-Host "---Start---"
 $srcRootPath = (Get-Item * | Where-Object {$_.FullName.EndsWith("src")})
-$dir = Get-ChildItem -Path $srcRootPath -Directory
-foreach ($d in $dir){
-      $filePath = Join-Path -Path $d.FullName -ChildPath "task.json"
-      if (Test-Path $filePath){
-        #Write-Host "FilePath: $filePath"
 
-        $a = Get-Content $filePath -raw | ConvertFrom-Json
-        
-        $newFriendlyName = "$($a.friendlyName)-TEST"
-        $a.friendlyName = "$newFriendlyName"
+#$srcRootPath
+Write-Host "Root: $srcRootPath"
 
-        $newName = "$($a.name)-TEST"
-        $a.name = "$newName"
-        
-        $a | ConvertTo-Json -depth 32| set-content $filePath
-        Write-Host "Updated: $filePath"
-      }
+$tasksfiles = Get-ChildItem -Path $srcRootPath -Filter task.json -Recurse -ErrorAction SilentlyContinue -Force
+foreach ($taskfile in $tasksfiles){
+  Write-Host "Task file: $taskfile"
 }
+
+# $dir = Get-ChildItem -Path $srcRootPath -Directory
+# foreach ($d in $dir){
+#       $filePath = Join-Path -Path $d.FullName -ChildPath "task.json"
+#       if (Test-Path $filePath){
+#         #Write-Host "FilePath: $filePath"
+
+#         $a = Get-Content $filePath -raw | ConvertFrom-Json
+        
+#         $newFriendlyName = "$($a.friendlyName)-TEST"
+#         $a.friendlyName = "$newFriendlyName"
+
+#         $newName = "$($a.name)-TEST"
+#         $a.name = "$newName"
+        
+#         $a | ConvertTo-Json -depth 32| set-content $filePath
+#         Write-Host "Updated: $filePath"
+#       }
+# }
 Write-Host "---End---"
