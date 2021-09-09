d6de2e8d19963ef4d41ee6c6f58fc9a14573d9fa
Ove Lartelius <ove_lartelius@hotmail.com>
2021-09-09 16:16:34 +0200

    Removed Az.* modules from source

--- BuildScripts/CopyScripts.ps1
+++ BuildScripts/CopyScripts.ps1
@@ -2,7 +2,5 @@
 . "$PSScriptRoot\CopyEpinovaDxpDeploymentUtilScript.ps1"
 . "$PSScriptRoot\CopyEpiCloudModule.ps1"
 . "$PSScriptRoot\CopyVstsTaskSdkModule.ps1"
-. "$PSScriptRoot\CopyAzStorageModule.ps1"
-. "$PSScriptRoot\CopyAzAccountsModule.ps1"
 
 . "$PSScriptRoot\RemoveReadme.ps1"
