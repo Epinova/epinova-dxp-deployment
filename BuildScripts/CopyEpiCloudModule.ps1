7285d4e2cc7c260caabe42b9095ec2e3bdea9f69
Ove Lartelius <ove_lartelius@hotmail.com>
2021-11-16 19:38:40 +0100

    :sparkles: Added EpiCloud v1 support

--- BuildScripts/CopyEpiCloudModule.ps1
+++ BuildScripts/CopyEpiCloudModule.ps1
@@ -2,11 +2,8 @@
 Write-Host "---Start---"
 
 $module = (Get-Item -Path .\Modules\EpiCloud).FullName
-
 Write-Host $module
-
 #Get-ChildItem -Path .\src -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
-
 $srcRootPath = (Get-Item .\* | Where-Object {$_.FullName.EndsWith("src")})
 $dir = Get-ChildItem -Path $srcRootPath -Directory
 foreach ($d in $dir){
@@ -19,14 +16,39 @@ foreach ($d in $dir){
     else {
         # Handle if we have directories for versions.
         $subdir = Get-ChildItem -Path $d.FullName -Directory
-        foreach ($sd in $subdir){
-            $subfilePath = Join-Path -Path $sd.FullName -ChildPath "ps_modules"
-            
-            if (Test-Path $subfilePath){
-                Write-Host $subfilePath
-                Copy-Item -Path $module -Destination $subfilePath -Recurse -Force
+        #if ($subdir.EndsWith("V1") -or $subdir.EndsWith("V2")){
+            foreach ($sd in $subdir){
+                $subfilePath = Join-Path -Path $sd.FullName -ChildPath "ps_modules"
+                
+                if (Test-Path $subfilePath){
+                    Write-Host $subfilePath
+                    Copy-Item -Path $module -Destination $subfilePath -Recurse -Force
+                }
             }
-        }        
+        #}        
     }
 }
+
+#$module = (Get-Item -Path .\Modules\EpiCloudv1).FullName
+#Write-Host $module
+##Get-ChildItem -Path .\src -Directory -Force -ErrorAction SilentlyContinue | Select-Object FullName
+#$srcRootPath = (Get-Item .\* | Where-Object {$_.FullName.EndsWith("src")})
+#$dir = Get-ChildItem -Path $srcRootPath -Directory
+#foreach ($d in $dir){
+#    # Handle if we have directories for versions.
+#    $subdir = Get-ChildItem -Path $d.FullName -Directory
+#    if ($subdir.EndsWith("V3")){
+#        foreach ($sd in $subdir){
+#            $subfilePath = Join-Path -Path $sd.FullName -ChildPath "ps_modules"
+#            
+#            if (Test-Path $subfilePath){
+#                Write-Host $subfilePath
+#                Copy-Item -Path $module -Destination $subfilePath -Recurse -Force
+#                $oldName = Join-Path -Path $subfilePath -ChildPath "EpiCloudv1"
+#                $newName = Join-Path -Path $subfilePath -ChildPath "EpiCloud"
+#                Rename-Item $oldName $newName
+#            }
+#        }        
+#    }
+#}
 Write-Host "---End---"
