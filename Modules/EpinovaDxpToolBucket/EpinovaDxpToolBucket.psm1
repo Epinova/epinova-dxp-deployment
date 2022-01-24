ec2a36e6fbc91e0a2e18b2ca45e652b871289937
Ove Lartelius <ove_lartelius@hotmail.com>
2022-01-24 21:25:26 +0100

    :bug: Fixed backslash handling

--- Modules/EpinovaDxpToolBucket/EpinovaDxpToolBucket.psm1
+++ Modules/EpinovaDxpToolBucket/EpinovaDxpToolBucket.psm1
@@ -353,7 +353,7 @@ function Join-Parts {
         [string]$Separator = ''
     )
 
-    ($Parts | Where-Object { $_ } | ForEach-Object { ([string]$_).trim($Separator) } | Where-Object { $_ } ) -join $Separator 
+    ($Parts | Where-Object { $_ } | ForEach-Object { ([string]$_) } | Where-Object { $_ } ) -join $Separator 
 }
 
 function Get-DxpDateTimeStamp{
@@ -835,7 +835,7 @@ function Invoke-DownloadStorageAccountFiles{
                 break
             }
 
-            $filePath = Join-Parts -Separator '\' -Parts $DownloadFolder, $blobContent.Name
+            $filePath = (Join-Parts -Separator '\' -Parts $DownloadFolder, $blobContent.Name.Replace("/", "\"))
             $fileExist = Test-Path $filePath -PathType Leaf
 
             if ($fileExist -eq $false -or $true -eq $OverwriteExistingFiles){
