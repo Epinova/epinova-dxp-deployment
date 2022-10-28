175a642214d80d16558361b89a97a01bfc3d5dae
Ove Lartelius <ove_lartelius@hotmail.com>
2022-10-28 11:59:21 +0200

    :art: polish functions make tests

--- Scripts/HarmonizeDxpBlobToAzure_test.ps1
+++ Scripts/HarmonizeDxpBlobToAzure_test.ps1
@@ -2,66 +2,49 @@
 $DebugPreference = 'Continue'
 
 Remove-Module -Name "EpinovaDxpToolBucket" -Verbose
-Import-Module -Name C:\dev\epinova-dxp-deployment\Modules\EpinovaDxpToolBucket -Verbose
+Import-Module -Name E:\dev\epinova-dxp-deployment\Modules\EpinovaDxpToolBucket -Verbose
 
 Remove-Module -Name "EpinovaAzureToolBucket" -Verbose
-Import-Module -Name C:\dev\EpinovaAzureToolBucket-psmodule\Modules\EpinovaAzureToolBucket -Verbose
+Import-Module -Name E:\dev\EpinovaAzureToolBucket-psmodule\Modules\EpinovaAzureToolBucket -Verbose
 
-. C:\dev\temp\PowerShellSettingFiles\DxpProjects.ps1
+. E:\dev\temp\PowerShellSettingFiles\DxpProjects.ps1
 
 # [string] $clientKey = "xxx"
 # [string] $clientSecret = "xxx"
 # [string] $projectId = "xxx"
 [string] $DxpEnvironment = "Integration" #[ValidateSet('Integration','Preproduction','Production','ADE1','ADE2','ADE3')]
-[string] $DxpDatabaseDownloadFolder = "C:\dev\temp\_blobDownloads"
+[string] $DxpDownloadFolder = "E:\dev\temp\_blobDownloads"
 
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
 
-#Install-Module EpiCloud -Scope CurrentUser -MinimumVersion 1.2.0 -Force -AllowClobber
-Get-Module -Name EpiCloud -ListAvailable
-$ClientKey
-$ClientSecret
-$ProjectId
-$DxpEnvironment
-Import-Module EpiCloud
-$containers = Get-EpiStorageContainer -ClientKey $ClientKey -ClientSecret $ClientSecret -ProjectId $ProjectId -Environment $DxpEnvironment
-
 # Get a list of all containers for a environment so that we can download correct blobs.
-#$containers = Get-DxpStorageContainers -ClientKey $ClientKey -ClientSecret $ClientSecret -ProjectId $ProjectId -Environment $DxpEnvironment
-Write-Host "Containers:-------------------"
-$containers.storageContainers | Format-Table
-Write-Host "------------------------------"
+# $containers = Get-DxpStorageContainers -ClientKey $ClientKey -ClientSecret $ClientSecret -ProjectId $ProjectId -Environment $DxpEnvironment
+# Write-Host "Containers:-------------------"
+# $containers.storageContainers | Format-Table
+# Write-Host "------------------------------"
 
-# $files = Invoke-DxpBlobsDownload -ClientKey $clientKey -ClientSecret $clientSecret -ProjectId $projectId -Environment $DxpEnvironment -DownloadFolder $downloadFolder -MaxFilesToDownload 2 -Container "AppLogs"
+$DxpContainer = "mysitemedia"
 
-# # Temp set the downloaded database.
-# $filePath = "C:\dev\temp\_blobDownloads\epicms_Integration_20221027082506.bacpac"
+$files = Invoke-DxpBlobsDownload -ClientKey $ClientKey -ClientSecret $ClientSecret -ProjectId $ProjectId -Environment $DxpEnvironment -DownloadFolder $DxpDownloadFolder -MaxFilesToDownload 2 -Container $DxpContainer
 
-# if ($null -eq $filePath -or $filePath.Length -eq 0){
-#     Write-Host "We do not have any database to work with. Will exit."
-#     exit
-# }
+$files
 
-# [string] $SubscriptionId = "e872f180-979f-xxx-aff7-3bbxxxx7f89" 
-# [string] $ResourceGroupName = "rg-my-group"
-# [string] $StorageAccountName = "my-storage"
-# [string] $ContainerName = "db-backups"
+[string] $SubscriptionId = "e872f180-979f-xxx-aff7-3bbxxxx7f89" 
+[string] $ResourceGroupName = "rg-my-group"
+[string] $StorageAccountName = "my-storage"
+[string] $StorageAccountContainer = "db-backups"
 
-# . C:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_SendBlob.ps1
+. E:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_SendBlob.ps1
 
-# $BacpacFilename = Send-Blob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -ContainerName $ContainerName -FilePath $filePath #-Debug
+$StorageAccountContainer = "mysitemedia"
 
-# #$BacpacFilename = "epicms_Integration_20221027082506.bacpac"
-# $BacpacFilename
+foreach ($file in $files) {
+    $BlobName = $file.Replace($DxpDownloadFolder, "")
+    if ($BlobName.StartsWith("\")){
+        $BlobName = $BlobName.SubString(1, $BlobName.Length - 1)
+    }
+    #$BlobName
 
-# $SqlServerName = "your-sql-server"
-# $SqlDatabaseName = "your-sql-databasename"
-# $SqlDatabaseLogin = "sa"
-# $SqlDatabasePassword = "l#tm#inmyd@tabaseplease!"
-# $RunDatabaseBackup = $true
-# $SqlSku = "Basic"
-
-# # Override with real settings
-# . C:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_ImportBacpacDatabase.ps1
-
-# Import-BacpacDatabase -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageAccountContainer $StorageAccountContainer -BacpacFilename $BacpacFilename -SqlServerName $SqlServerName -SqlDatabaseName $SqlDatabaseName -SqlDatabaseLogin $SqlDatabaseLogin -SqlDatabasePassword $SqlDatabasePassword -RunDatabaseBackup $RunDatabaseBackup -SqlSku $SqlSku
+    $fileUploaded = Send-Blob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageAccountContainer $StorageAccountContainer -FilePath $file -BlobName $BlobName #-Debug
+    Write-Host "File is uploaded: $fileUploaded"
+}
--- Scripts/HarmonizeDxpDbToAzure_test.ps1
+++ Scripts/HarmonizeDxpDbToAzure_test.ps1
@@ -2,26 +2,26 @@
 $DebugPreference = 'Continue'
 
 Remove-Module -Name "EpinovaDxpToolBucket" -Verbose
-Import-Module -Name C:\dev\epinova-dxp-deployment\Modules\EpinovaDxpToolBucket -Verbose
+Import-Module -Name E:\dev\epinova-dxp-deployment\Modules\EpinovaDxpToolBucket -Verbose
 
 Remove-Module -Name "EpinovaAzureToolBucket" -Verbose
-Import-Module -Name C:\dev\EpinovaAzureToolBucket-psmodule\Modules\EpinovaAzureToolBucket -Verbose
+Import-Module -Name E:\dev\EpinovaAzureToolBucket-psmodule\Modules\EpinovaAzureToolBucket -Verbose
 
-. C:\dev\temp\PowerShellSettingFiles\DxpProjects.ps1
+. E:\dev\temp\PowerShellSettingFiles\DxpProjects.ps1
 
 # [string] $clientKey = "xxx"
 # [string] $clientSecret = "xxx"
 # [string] $projectId = "xxx"
 [string] $DxpEnvironment = "Integration" #[ValidateSet('Integration','Preproduction','Production','ADE1','ADE2','ADE3')]
 [string] $DxpDatabaseName = "epicms" #[ValidateSet('epicms','epicommerce')]
-[string] $DxpDatabaseDownloadFolder = "C:\dev\temp\_blobDownloads"
+[string] $DxpDatabaseDownloadFolder = "E:\dev\temp\_blobDownloads"
 
 Set-ExecutionPolicy -Scope CurrentUser Unrestricted
 #$filePath = Invoke-DxpDatabaseDownload -ClientKey $clientKey -ClientSecret $clientSecret -ProjectId $projectId -Environment $DxpEnvironment -DatabaseName $DxpDatabaseName -DownloadFolder $DxpDatabaseDownloadFolder -RetentionHours 2 -Timeout 1800
 #Write-Host "Downloaded database: $filePath"
 
 # Temp set the downloaded database.
-$filePath = "C:\dev\temp\_blobDownloads\epicms_Integration_20221027082506.bacpac"
+$filePath = "E:\dev\temp\_blobDownloads\epicms_Integration_20210303134513.bacpac"
 
 if ($null -eq $filePath -or $filePath.Length -eq 0){
     Write-Host "We do not have any database to work with. Will exit."
@@ -31,15 +31,17 @@ if ($null -eq $filePath -or $filePath.Length -eq 0){
 [string] $SubscriptionId = "e872f180-979f-xxx-aff7-3bbxxxx7f89" 
 [string] $ResourceGroupName = "rg-my-group"
 [string] $StorageAccountName = "my-storage"
-[string] $ContainerName = "db-backups"
+[string] $StorageAccountContainer = "db-backups"
 
-. C:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_SendBlob.ps1
+. E:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_SendBlob.ps1
 
 #Install-Module Az.Storage -RequiredVersion 9.0.1
+$BlobName = $filePath.Substring($filePath.LastIndexOf("\") + 1)
+$BlobName
 
-$BacpacFilename = Send-Blob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -ContainerName $ContainerName -FilePath $filePath #-Debug
+$BacpacFilename = Send-Blob -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageAccountContainer $StorageAccountContainer -FilePath $filePath -BlobName $BlobName #-Debug
 
-#$BacpacFilename = "epicms_Integration_20221027082506.bacpac"
+#$BacpacFilename = "epicms_Integration_20210303134513.bacpac"
 $BacpacFilename
 
 $SqlServerName = "your-sql-server"
@@ -50,6 +52,7 @@ $RunDatabaseBackup = $true
 $SqlSku = "Basic"
 
 # Override with real settings
-. C:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_ImportBacpacDatabase.ps1
+. E:\dev\temp\PowerShellSettingFiles\EpinovaAzureToolBucket_ImportBacpacDatabase.ps1
 
-Import-BacpacDatabase -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageAccountContainer $StorageAccountContainer -BacpacFilename $BacpacFilename -SqlServerName $SqlServerName -SqlDatabaseName $SqlDatabaseName -SqlDatabaseLogin $SqlDatabaseLogin -SqlDatabasePassword $SqlDatabasePassword -RunDatabaseBackup $RunDatabaseBackup -SqlSku $SqlSku
+Import-BacpacDatabase -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageAccountContainer $StorageAccountContainer -BacpacFilename $BlobName -SqlServerName $SqlServerName -SqlDatabaseName $SqlDatabaseName -SqlDatabaseLogin $SqlDatabaseLogin -SqlDatabasePassword $SqlDatabasePassword -RunDatabaseBackup $RunDatabaseBackup -SqlSku $SqlSku
+#Import-BacpacDatabase -SubscriptionId $SubscriptionId -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName -StorageAccountContainer $StorageAccountContainer -BacpacFilename $BacpacFilename -SqlServerName $SqlServerName -SqlDatabaseName $SqlDatabaseName -SqlDatabaseLogin $SqlDatabaseLogin -SqlDatabasePassword $SqlDatabasePassword -RunDatabaseBackup $RunDatabaseBackup -SqlSku $SqlSku
