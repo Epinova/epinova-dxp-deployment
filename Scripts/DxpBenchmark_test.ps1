3398e5c5045ac9855fc99603e83f9f0725bb76af
Ove Lartelius <ove.lartelius@epinova.se>
2023-03-13 23:01:39 +0100

    Add testdata

--- Scripts/DxpBenchmark_test.ps1
+++ Scripts/DxpBenchmark_test.ps1
@@ -23,11 +23,27 @@
 # Invoke-RestMethod -Method 'Post' -ContentType "application/json" -Uri $url -Body $json #-OutFile output.csv
 
 function Send-ContextInfo {
-    $url = "https://app-dxpbenchmark-3cpox1-inte.azurewebsites.net/PipelineRun"
+    #$url = "https://app-dxpbenchmark-3cpox1-inte.azurewebsites.net/PipelineRun"
+    $url = "https://localhost:7002/PipelineRun"
 
     $postParams = @{ 
+        "SessionId"="4bacdddc-807c-43d1-8e93-99c1dfff7670"
         "Task"="PStest"
-    
+        "TaskVersion"="1.2.10"
+        "Environment"="Integration"
+        "DxpProjectId"="bfea291e-6886-4904-a12f-d6c2e09dc3d5"
+        "OrganisationId"="438a6774-a643-4b82-a962-19a7553be261" #System.CollectionId
+        "OrganisationName"="Epinova-Sweden" #System.CollectionUri
+        "ProjectId"="12f9e23a-c64f-490e-b63c-54d70eac71cb" #System.TeamProjectId
+        "ProjectName"="Percytest" #System.TeamProject
+        "Branch"="development" #Build.SourceBranchName
+        "AgentOS"="windows-latest"#Agent.OS
+        "EpiCloudVersion"="v1.2.0" #Make sure that Initialize-EpiCload set variable that we can read.
+        "PowerShellVersion"="7.2.1" #$PSVersionTable
+        "PowerShellEdition"="pwsh" #$PSVersionTable
+        "Elapsed"="1000"
+        "Result"="Succeeded"
+        "FileSize"="100"
         }
     $json = $postParams | ConvertTo-Json
     Invoke-RestMethod -Method 'Post' -ContentType "application/json" -Uri $url -Body $json
